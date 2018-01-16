cff799dcb7f67aa089a6168eb4d93987d61932a2
Kimberly Huynh and Sara Hamilton <kimberly@>
2018-01-16 09:47:36 -0800

    implement js to return list of 3 favorite things

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+  $("#arrayForm").submit(function (event){
 
+    var favorites = [$("input#food").val(), $("input#book").val(), $("input#color").val(), $("input#animal").val(), $("input#language").val()];
 
+    console.log(favorites);
+
+    var someFavorites = [];
+    someFavorites.push(favorites[2],favorites[1],favorites[3]);
+    console.log(someFavorites);
+
+    $("#list").append("<li>" + someFavorites[0] + "</li>");
+    $("#list").append("<li>" + someFavorites[1] + "</li>");
+    $("#list").append("<li>" + someFavorites[2] + "</li>");
+
+  event.preventDefault();
+  });
 });
